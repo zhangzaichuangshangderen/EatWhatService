6b9fd7a6568dcc0c11aafe2a82086938312273a6
吴英铭 <wym>
2026-08-07 22:54:05 +0800

    1

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,14 +1,6 @@
 CREATE DATABASE IF NOT EXISTS eatwhat;
 USE eatwhat;
 
-CREATE TABLE `Counters` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `count` int(11) NOT NULL DEFAULT '1',
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
 CREATE TABLE `Ingredients` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
